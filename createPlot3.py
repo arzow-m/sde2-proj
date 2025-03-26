d1c7c4247b3f1284add30802cb1b858e5c589609
arzow-m <antswah@gmail.com>
2025-03-26 15:35:16 -0400

    Update createPlot3.py

--- createPlot3.py
+++ createPlot3.py
@@ -1,8 +1,6 @@
 '''
 createPlot3.py
 
-    Author(s): Teigen Crook (1331121), Arzow Maksum (1337424), Andrew McLean (1328723), Syed Raamis (1334868)
-
     Project: Team Project
     Date of last update: March 20, 2025
 
